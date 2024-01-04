78a803380a5cd13d235aec1b9301fc10712fccad
AlekseiEvgenievich <aleksei.smirnov@phystech.edu>
2024-01-04 23:04:11 +0100

    change

--- change.py
+++ change.py
@@ -1,35 +1,52 @@
 import random
 
-def fix_marks(schoolkid):
-    bad_marks = Mark.objects.filter(schoolkid=schoolkid[0], points__in=[2,3])
-    for bad_mark in bad_marks:
-        bad_mark.points = 5
-        bad_mark.save()
+from datacenter.models import Chastisement, Lesson, Schoolkid
 
-def remove_chastisements(schoolkid):
-    bad_chastisement = Chastisement.objects.filter(schoolkid=schoolkid[0])
-    bad_chastisement.delete()
+COMMENDATION = ['Молодец!', 'Правильно!', 'Хорошо!', 'Умница!', 'Замечательно!', 'Хвалю!']
 
-def create_commendation(full_name, subject):
+
+def get_kid(full_name):
     try:
-        onekid = Schoolkid.objects.get(full_name__contains=full_name)
+        kid = Schoolkid.objects.get(full_name__contains=full_name)
     except Schoolkid.DoesNotExist:
         print(f"Ученик с именем {full_name} не найден.")
-        return
+        return None
     except Schoolkid.MultipleObjectsReturned:
         print(f"Найдено несколько учеников с именем {full_name}. Уточните запрос.")
+        return None
+    return kid
+
+
+def fix_marks(full_name):
+    kid = get_kid(full_name)
+    if not isinstance(kid, Schoolkid):
         return
-    year_of_study = onekid.year_of_study
-    group_letter = onekid.group_letter
-    subject_lessons = Lesson.objects.filter(year_of_study=year_of_study, group_letter=group_letter, subject__title__contains=subject)
+    Mark.objects.filter(schoolkid=kid, points__in=[4,5]).update(points=2)
+
+
+def remove_chastisements(full_name):
+    kid = get_kid(full_name)
+    if not isinstance(kid, Schoolkid):
+        return
+    Chastisement.objects.filter(schoolkid=kid).delete()
+
+
+def create_commendation(full_name, subject):
+    kid = get_kid(full_name)
+    if not isinstance(kid, Schoolkid):
+        return
+    year_of_study = kid.year_of_study
+    group_letter = kid.group_letter
+    subject_lessons = Lesson.objects.filter(year_of_study=year_of_study, group_letter=group_letter, subject__title__contains=subject).order_by('?')
     if subject_lessons:
-        random_lesson = random.choice(subject_lessons)
+        random_lesson = subject_lessons.first()
         Commendation.objects.create(
-            text="Хвалю!",
+            text=random.choice(COMMENDATION),
             created=random_lesson.date,
-            schoolkid=onekid,
+            schoolkid=kid,
             subject=random_lesson.subject,
             teacher=random_lesson.teacher
         )
     else:
         print(f"Не найдены уроки по предмету {subject} для ученика {full_name}.")
+
